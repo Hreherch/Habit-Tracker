9ad6620db81d6e9616d34820b781c2b46fd5c895
Hreherch <hreherch@ualberta.ca>
2016-09-21 14:52:22 -0600

    Added image assets and changed the drawer activity
    Tested with android SDK24

--- app/src/main/java/me/bennhart/hreherch_habittracker/MainActivity.java
+++ app/src/main/java/me/bennhart/hreherch_habittracker/MainActivity.java
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_button_todo) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_button_history) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_button_statistics) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_reset_all) {
 
         }
 
